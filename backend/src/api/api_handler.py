871441579cd61126071f9a34f637f155019e8ff7
Lyu Xiaoteng <2427367956@qq.com>
2024-10-23 03:28:08 +0800

    Adds functionality to return formatted json. Currently the sample audio is the same as the one from user.

--- backend/src/api/api_handler.py
+++ backend/src/api/api_handler.py
@@ -1,4 +1,7 @@
 # src/api/api_handler.py
+import base64
+import json
+
 from aiohttp import web
 from .audio_converter import convert_webm_to_wav
 from .model.model_caller import make_grpc_request
@@ -26,4 +29,29 @@ async def return_topic_words_score(word: str, audio_input: bytes):
     
     # Simulate scoring based on the word and audio input (to be replaced with actual logic)
     print(f"Scoring for word '{word}' with audio input of size {len(audio_input)/1024/1024} megabytes")
-    return {"word": word, "score": 100}  # Return a dummy score of 100 for now
+    #return {"word": word, "score": 100}  # Return a dummy score of 100 for now
+
+    base64_audio = base64.b64encode(wav).decode('utf-8')
+
+    json_data = {
+        "match_code": "FW369",
+        "game_mode": "normal",
+        "username": "junkrat1",
+        "overall_score": response,
+        "detailed_results": [
+            {
+                "question": "我爱吃苹果",
+                "audio_file": base64_audio,
+                "score": response
+            }
+        ],
+        "sample_audio": [
+            {
+                "question": "我爱吃苹果",
+                "audio_file": base64_audio,
+            }
+        ]
+    }
+
+    return json_data
+
